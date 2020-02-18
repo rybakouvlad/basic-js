9d5cf34b4e80aec784765e0c7663234d45f8db84
Vlad Rybakou <rybakou@MacBook-Pro-Vlad.local>
2020-02-18 22:18:17 +0300

    feat: add dream-team task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,17 +1,14 @@
 module.exports = function countCats(matrix ) {
-  //console.log(matrix);
   let count = 0;
+  
   for(let i = 0; i < matrix.length; i++) {
     for(let j of matrix[i]){
         
       if(/^\^\^$/.test(j) == true){
-       // console.log(j);
         count++;
       }
     }
   }
-  console.log("kkkkkkkkk ----------------" + count);
+ 
   return parseInt(count);
-  throw 'Not implemented';
-  // remove line with error and write your code here
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,33 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
+module.exports = function createDreamTeam(members) {
+  let result = "";
+  let mas = [];
+  if (Array.isArray(members) == false) {
+    return false;
+  }
+
+  members.forEach(function(item, index) {
+    if (typeof item === "string") {
+      mas.push(item);
+    } else {
+    }
+  });
+
+  for (let i of mas) {
+    i = i.trim();
+
+    if (/(^[a-z])|(^[A-Z])/.test(i) == true) {
+      result = result.concat(i.substr(0, 1).toUpperCase());
+    }
+  }
+
+  if (result.length == 0) {
+    return false;
+  }
+
+  return result
+    .split("")
+    .sort()
+    .join("");
+
   // remove line with error and write your code here
 };
