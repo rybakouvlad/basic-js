8e3c2fa857faab56f41e1a73c13d000d71e4eadc
rybakouvlad <rybakou.vladik@gmail.com>
2020-02-25 05:12:26 +0300

    fix: what-season throw

--- src/what-season.js
+++ src/what-season.js
@@ -1,13 +1,18 @@
 module.exports = function getSeason( date) {
-  if(isNaN(date) === true){
-     throw 'Not implemented';
+  if(typeof date === "undefined"){
+    return "Unable to determine the time of year!";
   }
+   if(isNaN(date) === true){
+     console.log("kill");
+    
+      throw new Error();
+   }
   switch (true) {
     case  date.getMonth() === 11 || date.getMonth() === 1 || date.getMonth() === 0 :  return "winter";
     case date.getMonth() > 1 && date.getMonth() < 5 : return "spring";
     case date.getMonth() > 4 && date.getMonth() < 8 : return "summer";
     case date.getMonth() >8 && date.getMonth() < 12: return "autumn";
-    default : return /autumn|fall/;
+    default : return "/autumn|fall/";
   }
 
 };
