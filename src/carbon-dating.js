a6145233bd05db49ed034f9f84c067c6c4f67979
rybakouvlad <rybakou.vladik@gmail.com>
2020-02-25 04:22:42 +0300

    feat: add hanoi-tower task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,13 +2,5 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample( sampleActivity ) {
-  //throw 'Not implemented';
-  if( sampleActivity == undefined){
-    return false;
-  }
-  // console.log(sampleActivity);
-  // remove line with error and write your code here
-return sampleActivity;
-  
-  
+
 };
