3ab234768f6193de95b411e6e5ba21d2a3bc97da
rybakouvlad <rybakou.vladik@gmail.com>
2020-03-06 00:02:11 +0300

    feat: add simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,42 @@
 const chainMaker = {
+  result: [],
   getLength() {
-    throw 'Not implemented';
+    return this.result.length;
+
     // remove line with error and write your code here
   },
   addLink(value) {
-    throw 'Not implemented';
+    this.result.push("( " + value + " )");
+    return this;
+
+    //throw 'Not implemented';
     // remove line with error and write your code here
   },
   removeLink(position) {
-    throw 'Not implemented';
+    if (
+      typeof position === "number" &&
+      position <= this.result.length &&
+      position > 0
+    ) {
+      this.result.splice(position - 1, 1);
+      return this;
+    } else {
+      this.result = [];
+      throw new Error();
+    }
     // remove line with error and write your code here
   },
   reverseChain() {
-    throw 'Not implemented';
+    this.result.reverse();
+    return this;
     // remove line with error and write your code here
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let a = this.result.join("~~");
+
+    this.result = [];
+
+    return a;
   }
 };
 
