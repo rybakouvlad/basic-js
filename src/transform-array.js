b4d9a4660b0a2e663cd35fc52f6dca02875776aa
rybakouvlad <rybakou.vladik@gmail.com>
2020-09-25 05:35:49 +0300

    feat: add transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,44 +2,48 @@ module.exports = function transform(arr) {
   // throw 'Not implemented';
   // remove line with error and write your code here
   //console.log(arr);
+  let result = [];
   if (!Array.isArray(arr)) {
     throw Error("arr is not Array");
   }
   if (arr.length === 0) {
     return [];
   }
-  for(let i = 0; i < arr.length; i++) {
-    if (arr[i + 1] === "--double-next" || i === arr.length - 1) {
-      arr.splice(i, 1);
-      
-    } else {
-      arr[i] = arr[i + 1];
-      
-    }
-    if (arr[i - 1] === "--double-prev" || i === 0) {
-      arr.splice(i, 1);
-     
-    } else {
-      arr[i] = arr[i - 1];
-    }
-  }
 
-  for(let i = 0; i < arr.length;i++){
-    if (arr[i + 1] === "--discard-next" || i === arr.length - 1) {
-      arr.splice(i, 1);
-     
-    } else {
-      arr.splice(i, 2);
-     
-    }
-    if (arr[i - 1] === "--discard-prev" || i === 0) {
-      arr.splice(i, 1);
-   
-    } else {
-      arr.splice(i - 1, 2);
-     
+  for (let i = 0; i < arr.length; i++) {
+    // console.log(arr[i]);
+    //console.log(result);
+    switch (arr[i]) {
+      case "--discard-next":
+        if (arr[i + 2] === "--double-prev" || arr[i + 2] === "--double-prev") {
+          result.push(arr[i + 1]);
+          i++;
+          i++;
+          break;
+        }
+        i++;
+        break;
+      case "--discard-prev":
+        result.splice(result.length - 1, 1);
+        break;
+      case "--double-next":
+        if (i >= arr.length - 1) {
+          break;
+        }
+        result.push(arr[i + 1], arr[i + 1]);
+        i++;
+        break;
+      case "--double-prev":
+        if (result.length < 1) break;
+
+        result.push(result[result.length - 1]);
+        if (arr[i - 2] === "--discard-next") {
+        }
+        break;
+      default:
+        result.push(arr[i]);
+        break;
     }
   }
-  
-  return arr;
+  return result;
 };
